507046b6807583763770aa580a5044a88d7a0ef6
okkdev <dev@stehlik.me>
2021-12-01 22:15:54 +0100

    🎄 day 1

---
+++ day1.hs
@@ -0,0 +1,21 @@
+part1 :: [Int] -> Int
+part1 xs = foldl (\acc (x, y) -> if x < y then acc+1 else acc) 0 . zip xs $ tail xs
+
+part2 :: [Int] -> Int
+part2 = part1 . go
+  where 
+    go [] = []
+    go xs = (sum $ take 3 xs) : go (drop 1 xs)
+
+main :: IO ()
+main = do
+  input <- getInput
+  putStrLn "Part 1:"
+  print $ part1 input
+  putStrLn "Part 2:"
+  print $ part2 input
+
+getInput :: IO [Int]
+getInput = do
+  contents <- readFile "input.txt"
+  return . map read . lines $ contents
