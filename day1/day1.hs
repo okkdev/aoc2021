66908dfb4277f0244b62c07ba212929b86e2b14c
okkdev <dev@stehlik.me>
2021-12-01 23:07:41 +0100

    smaller readfile

--- day1/day1.hs
+++ day1/day1.hs
@@ -1,3 +1,5 @@
+module Day01 where
+
 part1 :: [Int] -> Int
 part1 xs = foldl (\acc (x, y) -> if x < y then acc+1 else acc) 0 . zip xs $ tail xs
 
@@ -9,13 +11,8 @@ part2 = part1 . go
 
 main :: IO ()
 main = do
-  input <- getInput
+  input <- map read . lines <$> readFile "input.txt"
   putStrLn "Part 1:"
   print $ part1 input
   putStrLn "Part 2:"
   print $ part2 input
-
-getInput :: IO [Int]
-getInput = do
-  contents <- readFile "input.txt"
-  return . map read . lines $ contents
